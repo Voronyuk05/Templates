d3f34225dc0f8885d71e06ffab6a60b1be2ac772
Ivan <vandoslando@gmail.com>
2023-07-09 22:27:33 +0300

    file v1.0

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import usersData from './users.json';
+import usersItemTpl from './templates/gallery-item.handlebars'
+const Filter = document.querySelector('.filter-list')
+const list = document.querySelector('.js-gallery')
+const filteredUsers = usersData.filter(user => user.age > 0);
+function templateUsers(filteredUsers) {
+    const compiledTemplate = usersItemTpl({
+        user: filteredUsers,
+    });
+    list.innerHTML = compiledTemplate
+}
+templateUsers(filteredUsers)
+
+//Filter buttons callbacks
+Filter.addEventListener('click', filterUsers)
+function filterUsers(event) {
+    if (event.target.classList.contains('standart-filter')) {
+        const filteredUsers = usersData.filter(user => user.age > 0);
+        templateUsers(filteredUsers)
+    }
+    if (event.target.classList.contains('years-filter-btn')) {
+        const filteredUsers = usersData.filter(user => user.age >= 30);
+        templateUsers(filteredUsers)
+    }
+}
+
